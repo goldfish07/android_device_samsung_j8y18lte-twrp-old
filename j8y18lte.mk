962abc9048178f6292c6166508cc3f19ebea83d3
Ayush <ayushbisht5663@gmail.com>
2020-07-13 10:38:59 +0530

    j8y18lte: update build configs

--- j8y18lte.mk
+++ j8y18lte.mk
@@ -2,14 +2,14 @@
 PRODUCT_RELEASE_NAME := j8y18lte
 
 # Inherit some common omni stuff.
-$(call inherit-product, vendor/omni/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/j8y18lte/device_j8y18lte.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j8y18lte
-PRODUCT_NAME := omni_j8y18lte
+PRODUCT_NAME := j8y18lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := j8y18lte
 PRODUCT_MANUFACTURER := samsung
