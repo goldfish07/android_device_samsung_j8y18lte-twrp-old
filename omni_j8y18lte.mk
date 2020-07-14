1f68bc6f849c8668983d08543756b81c1f0ddab7
Ayush <ayushbisht5663@gmail.com>
2020-07-13 22:00:19 -0700

    j8y18lte: include board specific rc

--- omni_j8y18lte.mk
+++ omni_j8y18lte.mk
@@ -1,12 +1,22 @@
 # Release name
 PRODUCT_RELEASE_NAME := j8y18lte
 
+LOCAL_PATH := device/samsung/$(PRODUCT_RELEASE_NAME)
+
 # Inherit some common omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Resolution
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1480
+
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ramdisk/init.qcom.usb.rc:root/init.qcom.usb.rc \
+    $(LOCAL_PATH)/ramdisk/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
+    $(LOCAL_PATH)/ramdisk/ueventd.rc:root/ueventd.rc \
+
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j8y18lte
