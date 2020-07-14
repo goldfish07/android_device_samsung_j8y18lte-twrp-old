5c61cc3dcc3ac7201a344049b46fab097bad44c8
Ayush <ayushbisht5663@gmail.com>
2020-07-13 22:04:28 -0700

    j8y18lte: disable secure adb and enable adb debugging

--- omni_j8y18lte.mk
+++ omni_j8y18lte.mk
@@ -17,6 +17,29 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ramdisk/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
     $(LOCAL_PATH)/ramdisk/ueventd.rc:root/ueventd.rc \
 
+# Storage
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
+
+
+# Disable adb security
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	ro.mount.fs=EXT4 \
+	ro.allow.mock.location=0 \
+	ro.debuggable=1 \
+	ro.config.low_ram=false
+
+# For userdebug builds
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	ro.secure=0 \
+	ro.adb.secure=0 \
+	persist.sys.root_access=1 \
+	persist.service.adb.enable=1
+
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp,adb
+
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j8y18lte
